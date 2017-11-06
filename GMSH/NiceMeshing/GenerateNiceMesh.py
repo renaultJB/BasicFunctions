a0ac2509e89b81102f115fb303844eb0e651e1ef
renaultJB <j.baptiste.renault@gmail.com>
2017-11-06 13:49:59 +0100

    Working version of remeshing
    The *GenerateNiceMesh.py* was updated to generate patches. Patches  that are connected are merged. Then a temp .geo file is created to generate the "nice" 3D mesh

--- GMSH/NiceMeshing/GenerateNiceMesh.py
+++ GMSH/NiceMeshing/GenerateNiceMesh.py
@@ -12,8 +12,8 @@ mesh and results in bad elements.
 
 import os 
 import subprocess
-import GMSH as gmsh
-from collections import defaultdict
+from GMSH import Mesh
+import numpy as np
 
 CWD = os.getcwd()
 
@@ -30,50 +30,85 @@ CuttedCAD_Name = 'Tibia_GUI_R_cutted_a0.0.step'
 # =============================================================================
 
 subprocess.call(["cd", CWD], shell=True)
-subprocess.call(["gmsh", OriginalCAD_Name, '-2','-clmax','2'], shell=True)
-subprocess.call(["gmsh", CuttedCAD_Name, '-2','-clmax','2'], shell=True)
+subprocess.call(["gmsh", OriginalCAD_Name, '-2','-clmax','3'], shell=True)
+subprocess.call(["gmsh", CuttedCAD_Name, '-2','-clmax','3'], shell=True)
 
 # =============================================================================
 #  Import the recently created mesh
 # =============================================================================
   
-Mesh0 = gmsh.Mesh('.'.join(OriginalCAD_Name.split('.')[:-1])+'.msh')
-MeshC = gmsh.Mesh('.'.join(CuttedCAD_Name.split('.')[:-1])+'.msh')
+Mesh0 = Mesh('.'.join(OriginalCAD_Name.split('.')[:-1])+'.msh')
+MeshC = Mesh('.'.join(CuttedCAD_Name.split('.')[:-1])+'.msh')
 
 # =============================================================================
 # Check if the surfaces centroids of the cutted object belongs to the original
+# bone surfaces
 # =============================================================================
-
-OkSurf = defaultdict(list)
+small_surf_patch = []
+surf_areas = np.array([s.Area for s in MeshC.Surfaces])
+surf_cut = []
 
 for surf in MeshC.Surfaces:
     d, Nd = Mesh0.nearestNode(surf.Centroid)
-    if d < 3:
-        OkSurf[0].append(str(surf.Id))
+    
+    if d < 3 and surf.Area < 0.1*np.median(surf_areas):
+        small_surf_patch.append(surf.connected_surfaces(MeshC.Surfaces))
+    
+    elif d > 5:
+        surf_cut.append(surf.Id)
         
-for surf in MeshC.Surfaces:
-    if str(surf.Id) not in OkSurf[0] :
-        Areas = [s.Area for s in MeshC.Surfaces]
-        Areas.sort()
-        if surf.Area < Areas[-4]:
-            OkSurf[1].append(str(surf.Id))
-            
+# remove surfaces belonging to the cut surfaces
+small_patch_Ok = [p-set(surf_cut) for p in small_surf_patch]          
 
-f = open('Compound.geo','r')
-filedata = f.read()
 
-Lc = 's = news;\nCompound Surface(s)={{{}}};'
+# Merge set that intersect
+sets = list(small_patch_Ok)
+merged = True
+while merged:
+    merged = False
+    results = []
+    while sets:
+        common, rest = sets[0], sets[1:]
+        sets = []
+        for x in rest:
+            if x.isdisjoint(common):
+                sets.append(x)
+            else: 
+                merged = True
+                common |= x
+        results.append(common)
+    sets = results
 
-newdata = filedata.replace("Tibia.step" , CuttedCAD_Name)
-newdata = newdata.replace("LINE1" , Lc.format(','.join(OkSurf[0])))
-#newdata = newdata.replace("LINE2" , Lc.format(','.join(OkSurf[1])))
-newdata = newdata.replace("LINE2" , '')
-newdata = newdata.replace("LINE3" , '')
-finaldata = newdata.replace("Tibia.inp" , '.'.join(OriginalCAD_Name.split('.')[:-1]) + ".inp")
-f.close()
+# =============================================================================
+# Write found surface patches to .geo file to compound those surfaces
+# =============================================================================
+with open('Compound.geo','w') as f :
+    f.write('Mesh.RemeshAlgorithm=1;\n'
+            'Mesh.CharacteristicLengthExtendFromBoundary = 0;\n'
+            'Mesh.CharacteristicLengthFromPoints = 0;\n'
+            'Mesh.CharacteristicLengthFromCurvature = 0;\n'
+            'Mesh.CharacteristicLengthMin=1.05;\n'
+            'Mesh.CharacteristicLengthMax=2.25;\n'
+            )
+    
+    f.write('Merge "{}";'.format(CuttedCAD_Name))
+    
+    Lc = 's = news;\nCompound Surface(s)={{{}}};\n'
+    for s in sets :
+        s_str = [str(i) for i in s]
+        f.write(Lc.format(','.join(s_str)))
+    
+    
+    f.write('Mesh 2;\nMesh 3;\nOptimizeMesh "Gmsh";\nOptimizeMesh "Netgen";\n'
+            'SetOrder 2;\nSave "Tibia.inp";\nExit;')
+    
 
-f = open('Temp_inp.geo','w')
-f.write(finaldata)
-f.close()
 
-subprocess.call(["gmsh", 'Temp_inp.geo'], shell=True)
+# =============================================================================
+# Launch the the meshing of the coumpound surface
+# =============================================================================
+subprocess.call(["gmsh", 'Compound.geo'], shell=True)
+
+os.remove('Compound.geo')
+
+
